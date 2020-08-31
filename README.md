59f815bb5316b5013b8d8932530b4ee01bd17354
Chris Falk <christophersfalk@gmail.com>
2020-08-31 12:38:43 -0700

    added timer functionality. added to click start function. added additional credit for timer code found on stack overflow.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ coding exercise of generating a quiz for javascript.
 
 ## Additional Credits : 
 
+modified timer code from stackoverflow article (https://stackoverflow.com/questions/20239636/javascript-3-minute-timer-that-repeats-when-complete-and-shows-message-at-the-en)
+
 ### PsudoCode 
 
 on begin click 
--- script.js
+++ script.js
@@ -2,18 +2,22 @@
 let startQuizElement = document.querySelector(".starQuiz");
 let JumboElement = document.querySelector(".jumbotron")
 let switchedTextElement = document.querySelectorAll(".switchText");
+let questionElement = document.querySelector(".question");
+let timeDisplayedElement = document.querySelector(".timeRemaing")
+
+
 // Array of objects that will be stringified onto the page 
 const questions = [
     {
-    "question": "ladkfjdslkfj",
-    "optionA": "alfadlkfjdslkjf",
-    "optionB": "akjalfkjdafkljd",
-    "optionC": "aldkfjdklfj",
-    "optionD": "aflkjdslfkjsdlkfj",
-    "correctAnswer": this.optionA
+    question: "This is Question 1",
+    optionA: "This is answer one",
+    optionB: "This is answer two",
+    optionC: "This is answer three",
+    optionD: "this is answer four",
+    correctAnswer: this.optionA
     },
     {
-    "question": "ladkfjdslkfj",
+    question: "This is Question 2",
     "optionA": "alfadlkfjdslkjf",
     "optionB": "akjalfkjdafkljd",
     "optionC": "aldkfjdklfj",
@@ -51,12 +55,45 @@ const questions = [
 
 //i originally thought this would apply hide to all my items with the class but its not. 
 function beginQuiz() {
-    console.log(switchedTextElement);
     for (i = 0; i < switchedTextElement.length; i ++) {
         switchedTextElement[i].classList.add("hide");
     }
-    //switchedTextElement.forEach.add("hide");
-       
+    questionElement.classList.remove("hide")
+    //need to create a loop to generate question and buttons from first question array 
+    questionElement.textContent = questions[0].question;
+    
+    //being timer on click 
+    countdown(3, 0);      
+}
+
+//function takes in minutes and seconds as arguments 
+function countdown(minutes, seconds) {
+    // sets time in seconds
+    var time = minutes*60 + seconds;
+    //sets count down interval function for ever 1000 milisecond
+    var interval = setInterval(function() {
+        //sets a variable and grabs the timdisplayedelement
+        var el = timeDisplayedElement;
+        // if the time is 0 then end the counter
+        if(time == 0) {
+            //set up code to move onto displaying the score here 
+            el.innerHTML = "Move on to next date...";
+            //stops timer
+            clearInterval(interval);
+        }
+        // takes total number of seconds, divides by 60 to get minutes left, adds 0 if under ten
+        var minutes = Math.floor( time / 60 );
+        if (minutes < 10) minutes = "0" + minutes;
+        // takes total seconds and takes left over of division via modulous to get seconds remaining
+        var seconds = time % 60;
+        if (seconds < 10) seconds = "0" + seconds; 
+        //formats the timer nicely 
+        var text = minutes + ':' + seconds;
+        //writes the current time to the HTML doc
+        el.textContent = text;
+        //decreases the counter by 1
+        time--;
+    }, 1000);
 }
 
 
